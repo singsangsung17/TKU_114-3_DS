c45b3ac7cbddeb1ff97bf42eff4f0d55d64dc767
singsangsung17 <ivy0933969282@gmail.com>
2026-07-08 19:45:51 +0800

    Add files via upload

---
+++ 0707/WhileInputDemo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class WhileInputDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        while (number != 0) {
+            System.out.println(number);
+            number = sc.nextInt();
+        }
+    }
+}
