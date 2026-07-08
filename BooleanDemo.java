08235392b8ae5e729fd5e6f91d2ca9f4ef41fba3
singsangsung17 <ivy0933969282@gmail.com>
2026-07-08 19:37:53 +0800

    Delete BooleanDemo.java

--- BooleanDemo.java
+++
@@ -1,9 +0,0 @@
-public class BooleanDemo {
-    public static void main(String[] args) {
-        int score = 75;
-        boolean pass = score >= 60;
-
-        System.out.println("Score: " + score);
-        System.out.println("Pass: " + pass);
-    }
-}
